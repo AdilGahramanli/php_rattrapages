50fbc6b65cdbca393c4b28e17a30c10111fd7ef1
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2024-03-11 09:40:18 +0100

    database ok

--- affichageTests.php
+++ affichageTests.php
@@ -7,4 +7,8 @@ $item = new Item("nom", "description", 5, "image_Url", 15, 3, true);
 $name_item = $item -> getNom();
 echo($name_item);
 
+
+
+
+
 ?>
--- connexionBDD.php
+++ connexionBDD.php
@@ -2,7 +2,7 @@
 $servername = "localhost:3306";
 $username = "root";
 $password = "root";
-$dbname = "donjonsetdragons";
+$dbname = "databaserattrapages";
 
 // Créer la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,6 +13,27 @@ if ($conn->connect_error) {
 } else {
     echo "Connexion réussie !";
 }
+
+// Requête SQL
+$sql_select = "SELECT * FROM utilisateurs";
+$sql_insert = "INSERT INTO utilisateurs (nom, email) VALUES ('Jean Dupont', 'jean.dupont@exemple.com');";
+$sql_update = "UPDATE utilisateurs SET email = 'nouveau.email@exemple.com' WHERE id = 5";
+$sql_delete = "DELETE FROM utilisateurs WHERE actif = 0;"
+
+
+// Exécution de la requête
+$result_select = $mysqli->query($sql_select);
+$result_insert = $mysqli->query($sql_insert);
+$result_update = $mysqli->query($sql_update);
+$result_delete = $mysqli->query($sql_delete);
+
+
+
+
+
+
+
+
 
 // Fermer la connexion
 $conn->close();
